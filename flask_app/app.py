69b277d04ed467c2bceeadc6ed4290cdc502982b
Gabriel Majeau <127263958+gabmajeau@users.noreply.github.com>
2025-06-12 19:30:42 -0300

    Update app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,11 +1,12 @@
 
+import time
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/executar', methods=['POST'])
 def executar():
-    
+    time.sleep(5)
     return jsonify({'resultado': "Amo o professor harrison"})
 
 if __name__ == '__main__':
